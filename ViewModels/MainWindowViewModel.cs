f32c7120ff6792feb10c539da9e3acea6d3b7129
mikanevnikita2000 <86831041+mikanevnikita2000@users.noreply.github.com>
2021-12-02 20:13:49 +0300

    commit

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -8,37 +8,68 @@ namespace mather_2.ViewModels
     public class MainWindowViewModel : ViewModelBase
     {
         private string answer = "0";
-        private string _visibleExpression = "0"; 
-        private string correct_answer_or_not = "false";
+        private string _visibleExpression = "0";
+        private string _correct_answer_or_not = "false";
         private string _num1 = "0";
         private string _num2 = "0";
+        private string _resultat = "";
 
-
-        public void Click()
+        public void Start()
         {
             int num1 = 0;
             int num2 = 0;
-            int answer = 0;
-            string visibleExpression = "";
-            int resultat = 0;
-            bool correct_answer_or_not = false;
             Random g = new Random();
             num1 = g.Next(100);
             num2 = g.Next(100);
-
-
+            int pressing = 0;
+            string visibleExpression = "";
             visibleExpression = $"{num1}+{num2}";
-            answer = num1 + num2;
+            VisibleExpression = visibleExpression.ToString();
+            
+            pressing = Click();
+            
+            if (pressing==2)
+            {
+                Click1(num1, num2);
+            }
+        }
+        public int Click()
+        {
+
+            int pressing = 0;
+            if (pressing==2)
+            {
+                pressing = 0;
+            }
+            pressing = pressing + 1;
+            
+            return pressing;
+        }
+        public void Click1(int num1, int num2)
+        {
+            int answer = 0;
+            int resultat = 0;
+            Random g = new Random();
+            answer = Convert.ToInt32(Answer);
+            resultat = num1 + num2;
             if (resultat == answer)
             {
-                correct_answer_or_not = true;
+                Сorrect_answer_or_not = "true";
             }
+
+            Сorrect_answer_or_not = "привет";
         }
 
         public string Num
         {
             get => _num1;
             set => this.RaiseAndSetIfChanged(ref _num1, value);
+        }
+
+        public string Resultat
+        {
+            get => _resultat;
+            set => this.RaiseAndSetIfChanged(ref _resultat, value);
         }
 
 
@@ -55,8 +86,8 @@ namespace mather_2.ViewModels
         }
         public string Сorrect_answer_or_not
         {
-            get => correct_answer_or_not;
-            set => this.RaiseAndSetIfChanged(ref correct_answer_or_not, value);
+            get => _correct_answer_or_not;
+            set => this.RaiseAndSetIfChanged(ref _correct_answer_or_not, value);
         }
         public string VisibleExpression
         {
